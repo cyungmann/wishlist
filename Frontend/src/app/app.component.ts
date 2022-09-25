dd764fa6837a292cf8132faede5870f9232d88af
Chris Yungmann <yungmann.chris@gmail.com>
2022-09-24 22:43:57 -0500

    run `ng lint --fix`

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,15 +4,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public forecasts?: WeatherForecast[];
 
   constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+    http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+      result => {
+        this.forecasts = result;
+      },
+      error => console.error(error),
+    );
   }
 
   title = 'Wishlist';
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,13 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, HttpClientModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
